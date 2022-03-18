cf81d3551310be92aea17e3110f9aade29fc5481
VictoriaBinkley <vsbinkle@millersville.edu>
2022-03-17 23:01:02 -0400

    moon across house

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,3 +1,10 @@
+// state
+let circleX = 100;
+let circleY = 0;
+let xSpeed = 1;
+let ySpeed = 1;
+
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -18,13 +25,30 @@ fill(204,0,0);
   rect(230,65,35,75);
   rect(200,225,45,75);
 
+//doorknob
 fill(255,255,0);
   circle(230,265,10);
 
+//windows
 fill(224,224,224);
   square(145,175,40);
-
   square(145,230,40);
+
+//Moon
+fill(255,255,204);
+   // draw current frame based on state
+  circle(circleX, circleY, 100);
+
+  // modify state
+  circleX = circleX + xSpeed;
+  circleY = circleY + ySpeed;
+
+  //bounce off left and right
+  if(circleX < 0 || circleX > width) {
+    xSpeed = xSpeed * -1;
+  }
 
 }
 
+
+ 
