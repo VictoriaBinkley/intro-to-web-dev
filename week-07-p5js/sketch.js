42c8c420aae002fb42b988524ae80dd2e5443f2d
VictoriaBinkley <vsbinkle@millersville.edu>
2022-03-17 22:43:51 -0400

    with color

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -5,20 +5,26 @@ function setup() {
 function draw() {
   background(220);
 
-square(125,150,150);
+//house base
+fill(255,255,153);
+  square(125,150,150);
 
-triangle(300,165,200,70,100,160); 
+//Roof
+fill(128,128,128);
+  triangle(300,160,200,70,100,160); 
 
-rect(230,65,35,75);
+//chimney and door
+fill(204,0,0);
+  rect(230,65,35,75);
+  rect(200,225,45,75);
 
-rect(195,220,45,75);
+fill(255,255,0);
+  circle(230,265,10);
 
-circle(230,265,10);
+fill(224,224,224);
+  square(145,175,40);
 
-square(145,175,40);
-
-square(145,230,40);
-
-
+  square(145,230,40);
 
 }
+
