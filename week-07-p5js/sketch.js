1228f44016bed1b35535032eb543811c5499f5e2
VictoriaBinkley <vsbinkle@millersville.edu>
2022-03-17 22:08:42 -0400

    Drawing working

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -4,4 +4,21 @@ function setup() {
 
 function draw() {
   background(220);
+
+square(125,150,150);
+
+triangle(300,165,200,70,100,160); 
+
+rect(230,65,35,75);
+
+rect(195,220,45,75);
+
+circle(230,265,10);
+
+square(145,175,40);
+
+square(145,230,40);
+
+
+
 }
