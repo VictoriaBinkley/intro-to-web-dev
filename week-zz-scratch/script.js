1329dac7f6918c1b7477c63db32dffc683581218
VictoriaBinkley <vsbinkle@millersville.edu>
2022-05-04 12:28:55 -0400

    added ? box

--- week-zz-scratch/script.js
+++ week-zz-scratch/script.js
@@ -1,33 +1,17 @@
-//Cap America: The First avenger
-//captain marvel
-//Iron Man 
-//Iron Man 2
-//The Incredible Hulk 
-//Thor
-//The Avengers
-//Iron Man 3
-//Thor: Dark World 
-//Captain America: The Winter Soldier
-//Guardians of the Galaxy 
-//Guardians of the Galaxy Vol 2
-//Avengers: Age of Ultra 
-//Ant-Man
-//Captain America: Civil War
-//Spiderman: Homecoming
-//Doctor Strange
-//Black Panther 
-//Thor: Ragnarok
-//Ant-Man and the Wasp 
-//Black Widow 
-//Avengers: Infinity War
-//Avengers: Endgame
-//WandaVision
-//SHang-chi and the legend of the Ten Rings
-//Spiderman: Far From Home 
-//The Falcon and the Winter Soldier 
-//Eternals
-//Spider-Man: No Way Home 
-//Hawkeye
-//Moon Knight
-//Doctor Strange in the multiverse of Madness
-//Loki
+function fillOutputs() {
+
+  // Get the adjective input element
+  const adjectiveOneInput = document.getElementById('adjective-one-input');
+  // Get the text from the input element
+  const adjectiveOne = adjectiveOneInput.value;
+
+  // Get the adjective output element
+  const adjectiveOneOutput = document.getElementById('adjective-one-output');
+  // Set the text of the input element
+  adjectiveOneOutput.innerText = adjectiveOne;
+
+  // Get the output container element
+  const outputDiv = document.getElementById('output');
+  // Make the output container element visible
+  outputDiv.style.display = 'block';
+}
